5bb36732e84832ca5a5dd8944d707de159b40925
Kali <kali@example.com>
2026-01-22 14:00:03 +0800

    Add resource group

--- infra/init.tf
+++ infra/init.tf
@@ -1,24 +1,24 @@
 # Terraform and Azure provider block declarations will go in this file.
 
 terraform {
-    required_version = ">= 1.14.0"
+  required_version = ">= 1.14.0"
 
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = ">= 4.57.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.57.0"
     }
+  }
 
-    backend "remote" {
-        # The `hostname` parameter will default to "app.terraform.io".
-        organization = "forsakenidol-organization-1"
+  backend "remote" {
+    # The `hostname` parameter will default to "app.terraform.io".
+    organization = "forsakenidol-organization-1"
 
-        workspaces {
-            # If you specify a workspace that does not exist, it is automatically created in the Terraform Cloud.
-            name = "university-rankings"
-        }
+    workspaces {
+      # If you specify a workspace that does not exist, it is automatically created in the Terraform Cloud.
+      name = "university-rankings"
     }
+  }
 
 }
 
---
+++ infra/main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = "terraform-university-rankings-project-resources"
+  location = "Australia East"
+}
+
