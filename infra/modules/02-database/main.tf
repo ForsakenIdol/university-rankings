964e699b05b4a920064c17738f2289fa36baf0b0
Kali <kali@example.com>
2026-01-22 16:50:07 +0800

    Update Terraform rules to allow most public traffic to my mssql instance

--- infra/modules/02-database/main.tf
+++ infra/modules/02-database/main.tf
@@ -9,11 +9,21 @@ resource "azurerm_mssql_server" "sql_server" {
   # No `azuread_administrator` block means no Microsoft Entra authentication is configured for this resource.
 }
 
-resource "azurerm_mssql_firewall_rule" "global_allow" {
-  name             = "AllowAllTraffic"
+# This rule allows Azure services only.
+resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
+  name             = "AllowAzureServices"
   server_id        = azurerm_mssql_server.sql_server.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
+}
+
+# This rule allows a broader range of public access. Azure does not support a "global access" rule,
+# so we need to approximate global access with a broad firewall range.
+resource "azurerm_mssql_firewall_rule" "allow_public_traffic" {
+  name             = "AllowMostPublicTraffic"
+  server_id        = azurerm_mssql_server.sql_server.id
+  start_ip_address = "1.0.0.0"
+  end_ip_address   = "254.255.255.0"
 }
 
 resource "azurerm_mssql_database" "mssql_database" {
