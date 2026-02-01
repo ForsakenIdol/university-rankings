bd18b459fc9c77904029e583dbcb25ec2f1b4b22
Kali <kali@example.com>
2026-02-01 14:52:39 +0800

    Change some resource names and trigger azcopy

--- infra/modules/02-database/main.tf
+++ infra/modules/02-database/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_mssql_server" "sql_server" {
-  name                = "tf-university-rankings-sql-server"
+  name                = "tf-uni-rankings-sql-server"
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
   version             = "12.0"
--- infra/modules/03-data-factory-and-linked-services/main.tf
+++ infra/modules/03-data-factory-and-linked-services/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_data_factory" "data_factory" {
-  name                = "tf-university-rankings-data-factory"
+  name                = "tf-uni-rankings-data-factory"
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
 
@@ -16,7 +16,7 @@ resource "azurerm_data_factory" "data_factory" {
 ######################### Linked Services #########################
 
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "blob_storage_link" {
-  name                 = "tf-university-rankings-blob-storage-link"
+  name                 = "tf-uni-rankings-blob-storage-link"
   data_factory_id      = azurerm_data_factory.data_factory.id
   connection_string    = "DefaultEndpointsProtocol=https;AccountName=${var.storage_account_name};AccountKey=${var.storage_account_primary_access_key};EndpointSuffix=core.windows.net"
   storage_kind         = "StorageV2"
@@ -24,7 +24,7 @@ resource "azurerm_data_factory_linked_service_azure_blob_storage" "blob_storage_
 }
 
 resource "azurerm_data_factory_linked_service_sql_server" "sql_server_link" {
-  name              = "tf-university-rankings-sql-server-link"
+  name              = "tf-uni-rankings-sql-server-link"
   data_factory_id   = azurerm_data_factory.data_factory.id
   # Reference for Encrypt=True and TrustServerCertificate=False: https://learn.microsoft.com/en-us/azure/azure-sql/database/security-overview?source=recommendations&view=azuresql#information-protection-and-encryption
   connection_string = "Server=tcp:${var.sql_server_fdqn},1433;Initial Catalog=${var.database_name};User ID=${var.database_admin_username};Password=${var.database_admin_password};Encrypt=True;TrustServerCertificate=False;"
